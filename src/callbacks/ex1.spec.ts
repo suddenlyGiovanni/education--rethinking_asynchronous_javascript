7c52856b6fdafe6d8b86c259cad09451ead746b4
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2019-05-05 16:41:34 +0200

    feat(callbacks/ex1.ts): added a solution

---
+++ src/callbacks/ex1.spec.ts
@@ -0,0 +1,7 @@
+import { getFile } from './ex1'
+
+describe('getFile [callback]', () => {
+  test('getFile function is defined', () => {
+    expect(getFile).toBeDefined()
+  })
+})
--- src/callbacks/ex1.ts
+++ src/callbacks/ex1.ts
@@ -3,12 +3,12 @@ interface Cb<T> {
   (t: T): any
 }
 function fakeAjax(url: URI, cb: Cb<string>) {
-  var fake_responses = {
+  const fake_responses = {
     file1: 'The first text',
     file2: 'The middle text',
     file3: 'The last text',
   }
-  var randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000
+  const randomDelay = (Math.round(Math.random() * 1e4) % 8000) + 1000
 
   console.log('Requesting: ' + url)
 
@@ -24,11 +24,46 @@ function output(text: string): void {
 // **************************************
 // The old-n-busted callback way
 
-function getFile(file: URI) {
+export function getFile(file: URI) {
   fakeAjax(file, function(text) {
     // what do we do here?
-    output(text) //?
+    fileReceived(file, text)
   })
+}
+
+// hold responses in whatever order they come back
+const responses: {
+  file1?: string | true
+  file2?: string | true
+  file3?: string | true
+} = {}
+
+function fileReceived(file: URI, text: string) {
+  // haven't received this text yet?
+  if (!(file in responses)) {
+    responses[file] = text
+  }
+
+  const files: ['file1', 'file2', 'file3'] = ['file1', 'file2', 'file3']
+
+  // loop through responses in order for rendering
+
+  for (const _file of files) {
+    // response received?
+
+    if (_file in responses) {
+      if (responses[_file] !== true) {
+        // response needs to be rendered?
+        output(responses[_file])
+        responses[_file] = true
+      }
+      // can't render yet
+    } else {
+      // not complete!
+      return false
+    }
+  }
+  output('Complete!')
 }
 
 // request all files at once in "parallel"
